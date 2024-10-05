a58fb55db45228164e316f827b09e516c0e019cb
Harshil Doshi <harshildoshi153@gmail.com>
2024-10-05 10:54:50 +0530

    routing between signup and signin page

--- src/components/Demo/Auth/SignIn.jsx
+++ src/components/Demo/Auth/SignIn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {MdKeyboardArrowLeft} from 'react-icons/md';
 import Input from '../../../utils/input';
 
-const SignIn = () => {
+const SignIn = ({setSignReq}) => {
   return (
     <div className="size mt-[6rem] text-center">
       <h2 className="text-3xl">Sign in with email</h2>
@@ -10,11 +10,11 @@ const SignIn = () => {
         Enter the email address associated with your account, and we’ll send a
         magic link to your inbox.
       </p>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-2">
         <Input type="email" title="email" />
         <Input type="password" title="password" />
         <button
-          className={`px-4 py-1 text-sm rounded-full bg-green-700
+          className={`px-4 py-1 my-3 text-sm rounded-full bg-green-700
         hover:bg-green-800 text-white w-fit mx-auto`}>
           Sign In
         </button>
@@ -24,7 +24,7 @@ const SignIn = () => {
         className="mt-5 text-sm text-green-600 hover:text-green-700
       flex items-center mx-auto">
         <MdKeyboardArrowLeft />
-        All sign In Options
+        All Sign In Options
       </button>
     </div>
   )
--- src/components/Demo/Auth/SignUp.jsx
+++ src/components/Demo/Auth/SignUp.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import {MdKeyboardArrowLeft} from 'react-icons/md';
+import Input from '../../../utils/input';
 
-const SignUp = () => {
+const SignUp = ({setSignReq}) => {
   return (
-    <div>
-      Sign Up
+    <div className="size mt-[6rem] text-center">
+      <h2 className="text-3xl">Sign in with email</h2>
+      <p className="w-full sm:w-[25rem] mx-auto py-[3rem]">
+        Enter the email address associated with your account, and we’ll send a
+        magic link to your inbox.
+      </p>
+      <form className="flex flex-col gap-2">
+        <Input type="username" title="username" />
+        <Input type="email" title="email" />
+        <Input type="password" title="password" />
+        <Input type="repassword" title="repassword" />
+        <button
+          className={`px-4 py-1 my-3 text-sm rounded-full bg-green-700
+        hover:bg-green-800 text-white w-fit mx-auto`}>
+          Sign In
+        </button>
+      </form>
+      <button
+        onClick={() => setSignReq("")}
+        className="mt-5 text-sm text-green-600 hover:text-green-700
+      flex items-center mx-auto">
+        <MdKeyboardArrowLeft />
+        All Sign In Options
+      </button>
     </div>
   )
 }

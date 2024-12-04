cdd3abae1e14fa8573abcee35b20f7ea3a0b3ea1
Harshil Doshi <harshildoshi153@gmail.com>
2024-12-04 15:31:48 +0530

    Demo only display the 3 posts.

--- src/components/Home/Posts/Posts.jsx
+++ src/components/Home/Posts/Posts.jsx
@@ -3,11 +3,15 @@ import useFetch from '../../hooks/useFetch';
 import Loading from '../../Demo/Loading';
 import PostCard from './PostCard';
 
-const Posts = () => {
+const Posts = ({posts}) => {
   const {data, loading} = useFetch("posts");
   return (
-    <section className='flex flex-col gap-[2.5rem]'>
-      {loading ? <Loading/> : data.map((post,i) => <PostCard post={post} key={i}/>)}
+    <section className="flex flex-col gap-[2.5rem]">
+      {loading ? (
+        <Loading />
+      ) : (
+        data.slice(0, posts).map((post, i) => <PostCard post={post} key={i} />)
+      )}
     </section>
   )
 }
